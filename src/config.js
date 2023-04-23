bfa5f1b55dd8898f46b908d366b8fb0ce0c53fa5
mari <mari.17ymy@gmail.com>
2023-04-23 18:42:07 -0400

    config

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ import {config} from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || 8000
-export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_DATABASE = process.env.DB_DATABASE || 'bd_proy'
+export const DB_USER = process.env.DB_USER || 'admin'
+export const DB_PASSWORD = process.env.DB_PASSWORD || '123456789'
+export const DB_HOST = process.env.DB_HOST || 'database-1.cgpy6ihkutba.us-east-2.rds.amazonaws.com'
+export const DB_DATABASE = process.env.DB_DATABASE || 'database'
 export const DB_PORT = process.env.DB_PORT || 3306
 export const JET_SECRET = process.env.JET_SECRET || 'notasecreta'
 export const TOKEN_KEY = process.env.TOKEN_KEY 
